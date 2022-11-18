d3be606b42339d746afb4fc5af1c714af0d142f1
dmunoz <dmunoz@unl.edu.ec>
2022-11-18 08:57:50 -0500

    media

--- cfg/urls.py
+++ cfg/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from siscapru import  views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    print('x')
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- siscapru/views.py
+++ siscapru/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import render, redirect
 from django.shortcuts import render
-
+from django.http import HttpResponse as HR
 # Create your views here.
+def index(request):
+    #crear página propia o pasar a dmin
+    return redirect("/admin")
